71e3f3a572d0f638eb992ea2830e53d942c60674
Chad Jones <chadjones839@gmail.com>
2021-04-08 10:47:35 -0500

    using state in class based components:

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: "Chad", age: 33 },
-      { name: "Harvey", age: 77 },
+      { name: "Chud", age: 33 },
+      { name: "Harvard", age: 77 },
       { name: "Sarag", age: 30 }
     ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked')
+    // DON'T DO THIS: this.state.persons[0].name ="Chadz"
+    this.setState({
+      persons: [
+        { name: "Chad", age: 33 },
+        { name: "Harvey", age: 77 },
+        { name: "Sarah", age: 30 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>-- React App --</h1>
-        <button>
-          
-        </button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies: Whiskey</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
