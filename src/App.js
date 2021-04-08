cdfadf82faf29860a59fb19e5bb05be473ea8497
Chad Jones <chadjones839@gmail.com>
2021-04-08 10:12:06 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>-- React App --</h1>
+      <Person name="Chad" age="33" />
+      <Person name="Harvey" age="77" > My Hobbies: Whiskey</Person>
+      <Person name="Sarah" age="30" />
     </div>
   );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React App'));
 }
 
 export default App;
